380bc01fddf7602871ed0001085e9a65ea007d8e
nuruddin2971 <nuruddin2971@gmail.com>
2026-03-19 16:40:46 +0800

    added img

--- script/script.js
+++ script/script.js
@@ -1,6 +1,16 @@
 let currentTab = "alt";
+const tabActive = ["bg-navy", "border-navy"];
+const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200"];
 
 function switchTab(tab) {
   console.log(tab);
   const tabs = ["all", "interview", "rejected"];
+
+  for (const t of tabs) {
+    const tabName = document.getElementById("tab-" + t);
+    if (t === tab) {
+      tabName.classList.remove(...tabInactive);
+      tabName.classList.add(...tabActive);
+    }
+  }
 }

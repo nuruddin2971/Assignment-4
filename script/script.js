2b183a34acc80ec671b88104720dacf455fac5ba
nuruddin2971 <nuruddin2971@gmail.com>
2026-03-20 07:44:38 +0800

    caught total element

--- script/script.js
+++ script/script.js
@@ -2,6 +2,10 @@ let currentTab = "all";
 const tabActive = ["bg-blue-500", "border-blue-500", "text-white"];
 const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200"];
 
+const allContainer = document.getElementById("all-container");
+const interviewContainer = document.getElementById("interview-container");
+const rejectedContainer = document.getElementById("rejected-container");
+
 function switchTab(tab) {
   console.log(tab);
   const tabs = ["all", "interview", "rejected"];
@@ -16,4 +20,26 @@ function switchTab(tab) {
       tabName.classList.add(...tabInactive);
     }
   }
+
+  const pages = [allContainer, interviewContainer, rejectedContainer];
+  for (const section of pages) {
+    section.classList.add("hidden");
+  }
+
+  if (tab === "all") {
+    allContainer.classList.remove("hidden");
+  } else if (tab === "interview") {
+    interviewContainer.classList.remove("hidden");
+  } else {
+    rejectedContainer.classList.remove("hidden");
+  }
 }
+
+// stat update
+const totalStat = document.getElementById("stat-total");
+const interviewStat = document.getElementById("stat-interview");
+const rejectedStat = document.getElementById("stat-rejected");
+
+totalStat.innerText = allContainer.children.length;
+
+switchTab(currentTab);
